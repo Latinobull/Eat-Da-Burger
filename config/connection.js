766fc7c47e123c41bf9cafda62ee6892a67c6094
Donnahue George <Donnahuegjr@gmail.com>
2021-02-10 12:06:04 -0500

    getting connection working

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+require("dotenv").config();
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.DB_PASS,
+  database: "burgers_db",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+module.exports = connection;
