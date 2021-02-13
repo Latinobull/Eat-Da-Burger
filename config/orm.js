30c2535aa6d5d6a2b40b0cc19361deb4589607ac
Donnahue George <Donnahuegjr@gmail.com>
2021-02-13 13:24:45 -0500

    finished orm for updating

--- config/orm.js
+++ config/orm.js
@@ -10,6 +10,28 @@ const printQuestionMarks = (num) => {
   return arr.toString();
 };
 
+const objToSql = (ob) => {
+  const arr = [];
+
+  // Loop through the keys and push the key/value as a string int arr
+  for (const key in ob) {
+    let value = ob[key];
+    // Check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // If string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = `'${value}'`;
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(`${key}=${value}`);
+    }
+  }
+
+  // Translate array of strings to a single comma-separated string
+  return arr.toString();
+};
+
 const orm = {
   selectAll(tableInput, cb) {
     const queryString = `SELECT * FROM ${tableInput}`;
@@ -36,9 +58,15 @@ const orm = {
 
   updateOne(table, objColVals, condition, cb) {
     const queryString = `UPDATE ${table}`;
-
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+    console.log(queryString);
     connection.query(queryString, (err, res) => {
-      console.log("I work");
+      if (err) throw err;
+
+      cb(res);
     });
   },
 };
