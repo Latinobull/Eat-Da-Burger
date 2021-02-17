57f2ccd166f104e75cc1e976ec611ecfdc1a8a52
Donnahue George <Donnahuegjr@gmail.com>
2021-02-16 19:01:47 -0500

    installing JAWSDB server in my code

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql");
 require("dotenv").config();
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DB_PASS,
-  database: "burger_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "burger_db",
+  });
+}
 
 connection.connect((err) => {
   if (err) {
